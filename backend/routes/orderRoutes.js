a6aa8f4e11c4b9f05210544cb5271aade5292cfb
Tanish Sabane <150649314+tanishsabane07@users.noreply.github.com>
2026-05-16 20:17:32 +0530

    Delete backend directory

--- backend/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createOrder, getOrders, getOrderById } = require('../controllers/orderController');
-
-router.post('/', createOrder);
-router.get('/', getOrders);
-router.get('/:id', getOrderById);
-
-module.exports = router;
--- backend/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getProducts, getProductById } = require('../controllers/productController');
-
-router.get('/', getProducts);
-router.get('/:id', getProductById);
-
-module.exports = router;
